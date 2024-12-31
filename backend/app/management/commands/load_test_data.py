de95576c02df8393283e1a0efde7d163d8272918
Jason Swartz <jasonrswartz@gmail.com>
2024-12-31 14:48:37 -0700

    changed load example data command to not load any parking spots or vertices.

--- backend/app/management/commands/load_test_data.py
+++ backend/app/management/commands/load_test_data.py
@@ -29,28 +29,4 @@ class Command(BaseCommand):
                     f"  Created: Camera {camera.cam_num} in {building.name}"
                 )
 
-                # Create parking spots for the cameras
-                # Assign spots 1,2 to Camera 2 and 3,4 to Camera 3
-                start_spot_num = (cam_index - 2) * 2 + 1
-                for spot_index in range(start_spot_num, start_spot_num + 2):
-                    spot = ParkingSpot.objects.create(
-                        spot_num=spot_index, camera=camera
-                    )
-                    self.stdout.write(
-                        f"    Created: Spot {spot.spot_num} for Camera {camera.cam_num}"
-                    )
-
-                    # Create vertices for each parking spot
-                    vertices = [
-                        (10 * spot_index, 20 * spot_index),
-                        (10 * spot_index + 10, 20 * spot_index),
-                        (10 * spot_index + 10, 20 * spot_index + 10),
-                        (10 * spot_index, 20 * spot_index + 10),
-                    ]
-                    for x, y in vertices:
-                        vertex = Vertex.objects.create(spot=spot, x=x, y=y)
-                        self.stdout.write(
-                            f"      Created: Vertex ({vertex.x}, {vertex.y}) for Spot {spot.spot_num}"
-                        )
-
         self.stdout.write(self.style.SUCCESS("Database populated with dummy data!"))
