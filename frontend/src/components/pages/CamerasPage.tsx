a3f4c8a9e1c1e2223290d1a0a5c6489476536ea1
Jason Swartz <jasonrswartz@gmail.com>
2025-01-02 21:26:25 -0700

    feeds are now paginated with labels for camera numbers

--- frontend/src/components/pages/CamerasPage.tsx
+++ frontend/src/components/pages/CamerasPage.tsx
@@ -1,6 +1,7 @@
 import { useNavigate, useParams } from 'react-router-dom';
-import { Building } from '../../types';
-import { Button, Grid, AspectRatio, Image } from '@mantine/core'
+import { useState } from 'react';
+import { Building, Camera } from '../../types';
+import { Stack, Pagination, Grid, AspectRatio, Image, Center, Text, Group } from '@mantine/core'
 import Header from '../misc/Header'
 import { generateSlug } from '../misc/generateSlug';
 import no_image from "../../assets/no_image.jpeg";
@@ -12,31 +13,45 @@ interface BuildingsPageProps {
 function BuildingsPage({ buildings }: BuildingsPageProps) {
     const { buildingSlug } = useParams<{ buildingSlug: string }>();
     const navigate = useNavigate();
-    console.log(buildings[0]?.name.toLowerCase().replace(/ /g, ''))
-    const building = buildings.find((b) => generateSlug(b.name) === buildingSlug);
-    const cameras = building?.cameras.sort((a, b) => a.cam_num - b.cam_num) || []
+    const building: Building | undefined = buildings.find((b) => generateSlug(b.name) === buildingSlug);
+    const cameras: Camera[] = building?.cameras.sort((a, b) => a.cam_num - b.cam_num) || []
+
+    const [activePage, setPage] = useState(1);
+    const camsPerPage = 15
+    const numPages = Math.ceil(cameras.length / camsPerPage)
+    const displayedCameras: Camera[] = cameras.slice((activePage - 1) * 15, (activePage * 15))
+
 
     return (
         <div>
             <Header title={`Cameras for ${building?.name}`} home={false} />
-            <Grid columns={10} align='center' justify="flex-start" p='md'>
-                {cameras.map((camera) => (
-                    <Grid.Col span={2} key={camera.id} >
-                        <AspectRatio
-                            maw={1000}
-                            mx="auto"
-                            ratio={4 / 3}
-                        >
-                            <div
-                                style={{ cursor: 'pointer' }}
-                                onClick={() => navigate(`/building/${buildingSlug}/camera/${camera.cam_num}`)}
+            <Stack align="center">
+                <Grid columns={5} align='center' justify="flex-start" p='md'>
+                    {displayedCameras.map((camera) => (
+                        <Grid.Col span={1} key={camera.id} >
+                            <AspectRatio
+                                maw={1000}
+                                mx="auto"
+                                ratio={4 / 3}
                             >
-                                <Image src={camera.image?.image_url || no_image} />
-                            </div>
-                        </AspectRatio>
-                    </Grid.Col>
-                ))}
-            </Grid>
+                                <div
+                                    style={{ cursor: 'pointer' }}
+                                    onClick={() => navigate(`/building/${buildingSlug}/camera/${camera.cam_num}`)}
+                                >
+                                    <Image src={camera.image?.image_url || no_image} >
+                                    </Image>
+                                    <Group justify='space-between'>
+                                        <Text fz='xl'>{camera.cam_num}</Text>
+                                        <Text ta='right' c='gray'>{camera.MAC}</Text>
+                                    </Group>
+
+                                </div>
+                            </AspectRatio>
+                        </Grid.Col>
+                    ))}
+                </Grid>
+                <Pagination value={activePage} onChange={setPage} total={numPages} />
+            </Stack>
         </div>
     );
 };
