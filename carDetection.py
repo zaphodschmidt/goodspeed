433967ec1933d4330ac6627a666818757c8bfe3b
ZaphodSchmidt <Zapschmidt@hotmail.com>
2025-01-15 23:24:53 -0700

    before we add new model

--- carDetection.py
+++ carDetection.py
@@ -1,42 +1,46 @@
-from ultralytics import YOLO
 import os
-import imghdr
 from PIL import Image
+import cv2
+from ultralytics import solutions
+from ultralytics.solutions.parking_management import ParkingManagement
 
-# test_file = "./backend/media/uploads/camera_snapshot_20250114192003_cam5.jpg"
-# img = Image.open(test_file)
-# img.show()
+# PATH = './camera_snapshot_20250114210012_cam11.jpeg'
+PATH = './commercial-parking-lots.jpg'
+MODEL = 'yolov8n.pt'
+BOUNDING = './bounding_boxes.json'
+IMG_SAVE = './wow1.jpeg'
 
-def is_jpeg(file_path):
+
+def isJpeg(file_path):
     try:
         with Image.open(file_path) as img:
             return img.format == 'JPEG'
     except (IOError, OSError):
         return False
 
-PATH = './backend/media/uploads'
 def getDirs(path: str):
     files = []
     for fileName in os.scandir(path):
-        if fileName.is_file() and is_jpeg(fileName):
-            files.append(PATH+"/"+fileName.name)
+        if fileName.is_file() and isJpeg(fileName):
+            files.append(path+"/"+fileName.name)
     return files
 
-fileList = getDirs(PATH)
-print(fileList)
-model = YOLO('yolov8n.pt')
-results = model(source = fileList, show = True, conf = 0.4, save = True)
+def runParkingDetection(imagePath: str, boundingBox: str):
+    managment = ParkingManagement(model_path = MODEL, json_file = boundingBox)
+    imgBGR = cv2.imread(imagePath)
 
-# CAR_CLASS_ID = 2
+    if imgBGR is None:
+        print(f"Could not open {imagePath}")
+        return
+    
+    results = managment.model.track(imgBGR, persist = True, show = False)
 
-# for result in results:
-#     for box in result.boxes:
-#         cls_id = int(box.cls)
-#         conf = float(box.conf)
-#         x_min, y_min, x_max, y_max = box.xyxy[0]
+    if results and results[0].boxes:
+        output = managment.process_data(imgBGR)
+        cv2.imwrite(IMG_SAVE, output)
+        print("Saved img!!")
 
-#         if cls_id == CAR_CLASS_ID:
-#             print(f"Car detected with confidence {conf:.2f}")
-#             print(f"Bounding box: x_min={x_min:.2f}, y_min={y_min:.2f}, x_max={x_max:.2f}, y_max={y_max:.2f}\n")
-# for r in results:
-#     r.show()
+if __name__ == "__main__":
+    runParkingDetection(PATH, BOUNDING)
+    # solutions.ParkingPtsSelection()
+
