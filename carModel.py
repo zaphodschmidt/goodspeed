a622cbada1c3347321a837cebdc2591ca98d2b72
ZaphodSchmidt <Zapschmidt@hotmail.com>
2025-01-18 20:50:54 -0800

    update

--- carModel.py
+++ carModel.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO, checks, hub
-
 checks()
+
 hub.login('2a1a443b20b14cf834a90455212c8e2447527a7c22')
-model = YOLO('https://hub.ultralytics.com/models/so9Q3LUuAH4M7ycWY9mp')
+
+model = YOLO('https://hub.ultralytics.com/models/VeIcAf98OeRUZ4totalp')
 results = model.train()
