412d8bff9b6b5172be4a2acb7e064b428989bf81
Jason Swartz <jasonrswartz@gmail.com>
2025-01-21 08:37:12 +0000

    fix python version and get rid of dj url import.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from corsheaders.defaults import default_headers
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
