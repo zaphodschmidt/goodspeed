b329cc9cc5089737feb43f6700ba6a5bab23bd08
ZaphodSchmidt <Zapschmidt@hotmail.com>
2025-02-05 14:54:04 -0700

    got api calls working now have to integrate into backend

--- carModel.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO, checks, hub
-checks()
-
-hub.login('2a1a443b20b14cf834a90455212c8e2447527a7c22')
-
-model = YOLO('https://hub.ultralytics.com/models/VeIcAf98OeRUZ4totalp')
-results = model.train()
--- parkingGUI.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO, checks, hub, solutions
-
-
-solutions.ParkingPtsSelection()
