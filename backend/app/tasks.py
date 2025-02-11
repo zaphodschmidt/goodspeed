0f8f0845cdfc5d1e47fe1590c75bd292f7dd22f2
zapgawd <zaphodschmidt@gmail.com>
2025-02-10 21:45:34 -0700

    use building name and camera number as parameters for run_parking_detection, instead of inferring it from the image path.

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -4,8 +4,8 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 @shared_task
-def run_parking_detection(image_path):
+def run_parking_detection(image_path, cam_num, building_name):
     from .services.parking_management import ParkingManagement
     parking_management = ParkingManagement(model_path='yolov8n.pt')
-    parking_management.runParkingDetection(image_path)
+    parking_management.run_parking_detection(image_path, cam_num, building_name)
     logger.info(f"Spot detection completed for {image_path}")
