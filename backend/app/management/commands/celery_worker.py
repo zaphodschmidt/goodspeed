a4b436b4f28576d2711e1def3ed17bc59777c8de
Jason Swartz <jasonrswartz@gmail.com>
2025-02-15 22:13:09 -0700

    make it so only warning logs are shown in prod.

--- backend/app/management/commands/celery_worker.py
+++ backend/app/management/commands/celery_worker.py
@@ -1,9 +1,11 @@
 import shlex
 import subprocess
 import sys
-
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils import autoreload
+
+loglevel = 'warning' if settings.ENV == 'production' else 'info'
 
 
 def restart_celery():
@@ -12,7 +14,7 @@ def restart_celery():
     if sys.platform == "win32":
         cmd = "taskkill /f /t /im celery.exe"
     subprocess.call(shlex.split(cmd))
-    subprocess.call(shlex.split(f"{celery_worker_cmd} --loglevel=info"))
+    subprocess.call(shlex.split(f"{celery_worker_cmd} --loglevel={loglevel}"))
 
 
 class Command(BaseCommand):
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '1').lower() in ['true', '1', 'yes']
+ENV = os.getenv('ENV')
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
     
 # Application definition
